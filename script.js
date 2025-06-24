d6b9254d42110705e3698428641cd65d1d529998
Thunder9001 <30436382+Thunder9001@users.noreply.github.com>
2025-06-24 11:34:39 +0100

    Fixed calculator positioning, divvide by zero message addeede, cleaned up operator setting and display clearing

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const display = document.getElementById('display');
 const equButton = document.querySelector('#equals');
 let inputNum = 0;
+let intputFlag = false;
 let result = 0;
 let num1 = '';
 let num2 = '';
@@ -36,12 +37,24 @@ function operate(op, a, b) {
         case '/':
             return divide(a, b);
         default:
+            console.log(operator)
             throw new Error("Invalid operator");
     }
 }
 
+// Display update and clear functions.
 function updateDisplay(digit) {
+    if (inputFlag)
+    {
+        clearDisplay();
+        inputFlag = false;
+    }
     display.innerHTML += digit;
+}
+
+function clearDisplay() {
+    display.innerHTML = '';
+    inputFlag = false;
 }
 
 function resetNum() {
@@ -51,35 +64,36 @@ function resetNum() {
     operator = '';
 }
 
+function clearAll() {
+    clearDisplay();
+    resetNum();
+}
+
+
 function setOperator(op) {
-    if (operator !== '') {
-        evaluate();
-    }
-    if (inputNum !== '') {
-        inputNum = parseFloat(display.innerHTML);
+    inputNum = parseFloat(display.innerHTML);
+    if (inputNum != '') {
         operator = op;
         num1 = inputNum;
-        clearDisplay();
+        inputFlag = true;
     }
 }
 
-function clearDisplay() {
-    display.innerHTML = '';
-}
+
 function deleteLast() {
     display.innerHTML = display.innerHTML.slice(0, -1);
 }
 
 function evaluate(){
-    inputNum = parseFloat(display.innerHTML);
-    num2 = inputNum;
-    result = operate(operator, num1, num2);
-    display.innerHTML = result;
-    operator = '';
-    console.log(`Num1: ${num1}, Num2: ${num2}, Operator: ${operator}`);
-    num1 = result; // Update num1 to the result for subsequent operations
-    num2 = '';
-
+    if(num1 != '' && num2 != '' && operator != '')
+    {
+        inputNum = parseFloat(display.innerHTML);
+        num2 = inputNum;
+        display.innerHTML = result;
+        console.log(`Num1: ${num1}, Num2: ${num2}, Operator: ${operator}`);
+        result = operate(operator, num1, num2);
+        num1 = result; // Update num1 to the result for subsequent operations
+    }   
 }
 
 equButton.addEventListener('click', () => {
@@ -87,6 +101,5 @@ equButton.addEventListener('click', () => {
     num2 = inputNum;
     result = operate(operator, num1, num2);
     display.innerHTML = result;
-    operator = '';
     console.log(`Num1: ${num1}, Num2: ${num2}, Operator: ${operator}, Result: ${result}`);
 });
